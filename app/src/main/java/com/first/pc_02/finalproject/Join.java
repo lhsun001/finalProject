00cbeba0e25dd79b1bf64e38f6e0a135656e69bd
hatae1 <hatae4@naver.com>
2018-01-04 12:22:22 +0900

    Initial commit

---
+++ app/src/main/java/com/first/pc_02/finalproject/Join.java
@@ -0,0 +1,13 @@
+package com.first.pc_02.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
---
+++ app/src/main/java/com/first/pc_02/finalproject/Login.java
@@ -0,0 +1,39 @@
+package com.first.pc_02.finalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+    EditText login_id = null;
+    EditText login_pw = null;
+    Button login = null;
+    Button join = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_id = findViewById(R.id.login_id);
+        login_pw = findViewById(R.id.login_pw);
+        login = findViewById(R.id.login);
+        join = findViewById(R.id.join);
+
+
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_join = new Intent(Login.this,Join.class);
+                startActivity(go_join);
+
+            }
+        });
+
+
+
+
+    }
+}
